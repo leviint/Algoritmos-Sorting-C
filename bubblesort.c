f7343fe2789286ee87ffd65419dbf9b7170aa2a4
Arthur Bendini <arthurbendini@gmail.com>
2024-09-18 19:56:58 -0300

    Esqueleto do código das atividades adicionado.

---
+++ bubblesort.c
@@ -0,0 +1,8 @@
+// Faça um programa que receba valores aleatórios e diferentes em um vetor de inteiros de 6 posições
+// e implemente o bubble sort em uma função para ordenar este vetor.
+
+#include <stdio.h>
+
+int main(){
+    
+}
