83d6dd67e45bef8d72ea9ad22653d941f291160c
Arthur Bendini <arthurbendini@gmail.com>
2024-09-24 18:57:32 -0300

    Select Sort Concluído

--- bubblesort.c
+++ bubblesort.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-void bubbleSort(int array[], int tamanho){
+void bubbleSort(int array[], int numElementos){
     bool trocado;
-    int i;
     int temp;
 
     do{
         trocado = false;
-        for (i = 0; i < tamanho - 1; i++){
+        for (int i = 0; i < numElementos - 1; i++){
             if (array[i] > array[i + 1]){
                 temp = array[i];
                 array[i] = array[i + 1];
@@ -21,17 +20,16 @@ void bubbleSort(int array[], int tamanho){
 
 int main(){
     int array[6];
-    int i;
 
-    for(i = 0; i < 6; i++){
+    for(int i = 0; i < 6; i++){
         printf("Digite o %d° número: ", i + 1);
         scanf("%d", &array[i]);
     }
 
     bubbleSort(array, 6);
 
-    printf("Array ordenado: ");
-    for(i = 0; i < 6; i++){
+    printf("Array ordenado com BUBBLE SORT: ");
+    for(int i = 0; i < 6; i++){
         if(i < 5){
             printf("%d, ", array[i]);
         }else{
--- selectsort.c
+++ selectsort.c
@@ -2,7 +2,45 @@
 // e implemente o select sort em uma função para ordenar este vetor.
 
 #include <stdio.h>
+#include <stdbool.h>
+
+void selectSort(int array[], int numElementos){
+    int i, j;
+
+    for(i = 0; i < numElementos; i++){
+        int minimoIndex;
+        minimoIndex = i;
+        for(j = i + 1; j < numElementos; j++){
+            if(array[j] < array[minimoIndex]){
+                minimoIndex = j;
+            }
+        }
+        if(minimoIndex != i){
+            int temp = array[i];
+            array[i] = array[minimoIndex];
+            array[minimoIndex] = temp;
+        }
+    }
+}
 
 int main(){
-    
+    int array[6];
+
+    for(int i = 0; i < 6; i++){
+        printf("Digite o %d° número: ", i + 1);
+        scanf("%d", &array[i]);
+    }
+
+    selectSort(array, 6);
+
+    printf("Array ordenado com SELECTION SORT: ");
+    for(int i = 0; i < 6; i++){
+        if(i < 5){
+            printf("%d, ", array[i]);
+        }else{
+            printf("%d", array[i]);
+        }
+    }
+
+    return 0;
 }
