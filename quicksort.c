a91a26f6a6b74d7a9893b3ca0ba26e093d6a89c9
Arthur Bendini <arthurbendini@gmail.com>
2024-09-29 17:05:30 -0300

    Quick Sort concluído

--- quicksort.c
+++ quicksort.c
@@ -3,6 +3,41 @@
 
 #include <stdio.h>
 
+void quickSort(int array[], int low, int high){
+    int i = low, j = high, pivot = array[(low + high) / 2], temp;
+    while(i <= j){
+        while(array[i] < pivot) i++;
+        while(array[j] > pivot) j--;
+        if(i <= j){
+            temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+            i++;
+            j--;
+        }
+    }
+    if(low < j) quickSort(array, low, j);
+    if(i < high) quickSort(array, i, high);
+}
+
 int main(){
-    
+    int array[6];
+
+    for(int i = 0; i < 6; i++){
+        printf("Digite o %d° número: ", i + 1);
+        scanf("%d", &array[i]);
+    }
+
+    quickSort(array, 0, 5);
+
+    printf("Array ordenado com QUICK SORT: ");
+    for(int i = 0; i < 6; i++){
+        if(i < 5){
+            printf("%d, ", array[i]);
+        }else{
+            printf("%d", array[i]);
+        }
+    }
+
+    return 0;
 }
