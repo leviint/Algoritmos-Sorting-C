66f2061694dd03eb199ece3d0ff8093ef9a9d5a0
Arthur Bendini <arthurbendini@gmail.com>
2024-09-29 17:11:41 -0300

    Shell Sort concluído

--- shellsort.c
+++ shellsort.c
@@ -3,6 +3,36 @@
 
 #include <stdio.h>
 
+void shellSort(int array[], int numElementos){
+    for(int gap = numElementos / 2; gap > 0; gap /= 2){
+        for(int i = gap; i < numElementos; i++){
+            int temp = array[i];
+            int j;
+
+            for(j = i; j >= gap && array[j - gap] > temp; j -= gap){
+                array[j] = array[j - gap];
+            }
+            
+            array[j] = temp;
+        }
+    }
+}
+
 int main(){
-    
+    int array[6];
+
+    for(int i = 0; i < 6; i++){
+        printf("Digite o %d° número: ", i + 1);
+        scanf("%d", &array[i]);
+    }
+
+    shellSort(array, 6);
+    printf("Array ordenado com SHELL SORT: ");
+
+    for(int i = 0; i < 6; i++){
+        printf(i < 5 ? "%d, " : "%d", array[i]);
+    }
+
+
+    return 0;
 }
